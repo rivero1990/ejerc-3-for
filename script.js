4cb5ab86ed8ebcab0eed246d10c8b1404c8095e5
Anibal Rivero <riveroleonel@gmail.com>
2023-05-21 23:27:42 -0300

    modificacion ejercicio 3

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const mensajesRecicibidos = document.querySelector("#contenedor-msjs");
 
 function mensajesGenerados() {
     let eleccionMensajes = cantMensajes.value;
+    mensajesRecicibidos.innerHTML = "";
 
     for (let i = 0; i < eleccionMensajes; i++) {
         mensajesRecicibidos.innerHTML += "<h2>Javascript</h2>";
